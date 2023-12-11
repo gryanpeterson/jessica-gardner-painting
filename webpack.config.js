69c0a63267caa12d219a86593e7b3b06cbc95ba7
Ryan Peterson <gryanpeterson@gmail.com>
2023-12-11 09:29:46 -0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Jessica Gardner Painting",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
